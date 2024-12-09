4a65059a5ab43ad585ea0d2a76f5651bdae0d4bc
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2024-12-09 17:16:53 +0000

    Updated docker compose pull policy

--- scripts/00_vault_init.sh
+++ scripts/00_vault_init.sh
@@ -5,7 +5,7 @@ export VAULT_ADDR=http://127.0.0.1:8200
 vault status
 vault operator init -format=json | tee vault-init.json
 echo "Waiting for Vault to initialise..."
-sleep 30
+sleep 20
 echo "Unsealing vault..."
 export VAULT_TOKEN=$(cat vault-init.json | jq -r '.root_token')
 vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
@@ -14,3 +14,4 @@ vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
 vault status
 vault audit enable -path="audit_log" file file_path=/vault/logs/vault_audit.log
 vault audit enable -path="audit_stdout" file file_path=stdout
+echo "Vault initialised, unsealed and audit logs enabled"
